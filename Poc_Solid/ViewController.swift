30520d66a4159155171ec8f68f4ab2fb33e1e2c3
Maria <mariatupich@gmail.com>
2024-07-23 19:01:39 -0300

    first commit

---
+++ Poc_Solid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Poc_Solid
+//
+//  Created by Maria Tupich on 23/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
