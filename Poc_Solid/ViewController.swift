76dbcf700868d650cf10d652276c33eec5c3fc88
Maria <mariatupich@gmail.com>
2024-07-24 11:04:53 -0300

    creating files and folders initial strcuture

--- Poc_Solid/ViewController.swift
+++ Poc_Solid/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var studentsName: UITextField!
+    @IBOutlet weak var studentsGender: UITextField!
+    @IBOutlet weak var studentsEmail: UITextField!
+    @IBOutlet weak var studentsID: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func setupUIAdditionalConfiguration() {
+        
+    }
+    
+    
+    @IBAction func didTapConfirm(_ sender: UIButton) {
+    }
 }
+
+
 
