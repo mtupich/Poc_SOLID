26388ba24119861fd9e19a3efdb8605c1e1c5446
Maria <mariatupich@gmail.com>
2024-07-24 11:49:39 -0300

    layout adjustments

--- Poc_Solid/ViewController.swift
+++ Poc_Solid/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var studentsView: UIView!
     @IBOutlet weak var studentsName: UITextField!
     @IBOutlet weak var studentsGender: UITextField!
     @IBOutlet weak var studentsEmail: UITextField!
@@ -16,10 +17,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUIAdditionalConfiguration()
     }
     
     func setupUIAdditionalConfiguration() {
+        studentsView.layer.cornerRadius = 10 // Ajuste o raio conforme necessário
+        studentsView.layer.shadowColor = UIColor.black.cgColor
+        studentsView.layer.shadowOpacity = 0.50 // Ajuste a opacidade conforme necessário
+        studentsView.layer.shadowOffset = CGSize(width: 0, height: 2) // Ajuste o deslocamento conforme necessário
+        studentsView.layer.shadowRadius = 4 // Ajuste o raio conforme necessário
+        studentsView.layer.masksToBounds = false
         
+        studentsName.layer.borderWidth = 2
+        studentsName.layer.borderColor = UIColor.black.cgColor
+        studentsGender.layer.borderWidth = 2
+        studentsGender.layer.borderColor = UIColor.black.cgColor
+        studentsEmail.layer.borderWidth = 2
+        studentsEmail.layer.borderColor = UIColor.black.cgColor
+        studentsID.layer.borderWidth = 2
+        studentsID.layer.borderColor = UIColor.black.cgColor
     }
     
     
