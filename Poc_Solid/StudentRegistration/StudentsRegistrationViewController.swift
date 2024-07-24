c065e262ecd969fa32258d03e04084bc437e1c31
Maria <mariatupich@gmail.com>
2024-07-24 13:45:49 -0300

    creating the screens

--- Poc_Solid/StudentRegistration/StudentsRegistrationViewController.swift
+++ Poc_Solid/StudentRegistration/StudentsRegistrationViewController.swift
@@ -1,14 +1,13 @@
 //
-//  ViewController.swift
+//  StudentsRegistrationViewController.swift
 //  Poc_Solid
 //
-//  Created by Maria Tupich on 23/07/24.
+//  Created by Maria Tupich on 24/07/24.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class StudentsRegistrationViewController: UIViewController {
     @IBOutlet weak var studentsView: UIView!
     @IBOutlet weak var studentsName: UITextField!
     @IBOutlet weak var studentsGender: UITextField!
@@ -21,27 +20,24 @@ class ViewController: UIViewController {
     }
     
     func setupUIAdditionalConfiguration() {
-        studentsView.layer.cornerRadius = 10 // Ajuste o raio conforme necessário
+        studentsView.layer.cornerRadius = 10
         studentsView.layer.shadowColor = UIColor.black.cgColor
-        studentsView.layer.shadowOpacity = 0.50 // Ajuste a opacidade conforme necessário
-        studentsView.layer.shadowOffset = CGSize(width: 0, height: 2) // Ajuste o deslocamento conforme necessário
-        studentsView.layer.shadowRadius = 4 // Ajuste o raio conforme necessário
+        studentsView.layer.shadowOpacity = 0.50
+        studentsView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        studentsView.layer.shadowRadius = 4
         studentsView.layer.masksToBounds = false
-        
-        studentsName.layer.borderWidth = 2
+        studentsName.layer.borderWidth = 1
         studentsName.layer.borderColor = UIColor.black.cgColor
-        studentsGender.layer.borderWidth = 2
+        studentsGender.layer.borderWidth = 1
         studentsGender.layer.borderColor = UIColor.black.cgColor
-        studentsEmail.layer.borderWidth = 2
+        studentsEmail.layer.borderWidth = 1
         studentsEmail.layer.borderColor = UIColor.black.cgColor
-        studentsID.layer.borderWidth = 2
+        studentsID.layer.borderWidth = 1
         studentsID.layer.borderColor = UIColor.black.cgColor
     }
     
     
     @IBAction func didTapConfirm(_ sender: UIButton) {
     }
+
 }
-
-
-
---
+++ Poc_Solid/StudentsRegistrationMenu/StudentsRegistrationMenuViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StudentsRegistrationMenuViewController.swift
+//  Poc_Solid
+//
+//  Created by Maria Tupich on 24/07/24.
+//
+
+import UIKit
+
+class StudentsRegistrationMenuViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
